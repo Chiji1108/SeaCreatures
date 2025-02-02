ac0e24a84862cb24a4270f3d38b96de89d9dd09c
chiji1108 <shingo.pad@gmail.com>
2025-02-02 20:47:42 +0900

    Add interactive 3D model rotation in SeaCreatureDetailView
    - Implemented drag gesture to rotate 3D models horizontally and vertically
    - Added state variables to track rotation transformations
    - Enabled dynamic model interaction with smooth rotation tracking

--- SeaCreatures/SeaCreatureDetailView.swift
+++ SeaCreatures/SeaCreatureDetailView.swift
@@ -11,9 +11,27 @@ import SwiftUI
 
 struct SeaCreatureDetailView: View {
     let modelName: String
+    @State private var horizontalRotation = CGFloat.zero
+    @State private var verticalRotation = CGFloat.zero
+
+    @State private var endHorizontalRotation = CGFloat.zero
+    @State private var endVerticalRotation = CGFloat.zero
 
     var body: some View {
         Model3D(named: modelName, bundle: realityKitContentBundle)
+            .rotation3DEffect(.degrees(horizontalRotation), axis: .y)
+            .rotation3DEffect(.degrees(-verticalRotation), axis: .x)
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        horizontalRotation = value.translation.width + endHorizontalRotation
+                        verticalRotation = value.translation.height + endVerticalRotation
+                    }
+                    .onEnded { value in
+                        endHorizontalRotation = horizontalRotation
+                        endVerticalRotation = verticalRotation
+                    }
+            )
     }
 }
 
