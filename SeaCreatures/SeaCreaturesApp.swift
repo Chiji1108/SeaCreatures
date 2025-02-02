f7619795f65c146b9afa371ebd9852f9f68a61f7
chiji1108 <shingo.pad@gmail.com>
2025-02-02 19:50:53 +0900

    Initial Commit

---
+++ SeaCreatures/SeaCreaturesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeaCreaturesApp.swift
+//  SeaCreatures
+//
+//  Created by 千々岩真吾 on 2025/02/02.
+//
+
+import SwiftUI
+
+@main
+struct SeaCreaturesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
